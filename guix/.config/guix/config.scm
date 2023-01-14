2846abc4781f8abd52a62f0e920746079cb4d0a6
Hatim Thayyil <hatim@thayyil.net>
2023-01-14 01:59:35 +0000

    update guix channels and config

--- guix/.config/guix/config.scm
+++ guix/.config/guix/config.scm
@@ -1,81 +1,82 @@
+;; This is an operating system configuration generated
+;; by the graphical installer.
+;;
+;; Once installation is complete, you can learn and modify
+;; this file to tweak the system configuration, and pass it
+;; to the 'guix system reconfigure' command to effect your
+;; changes.
+
+
+;; Indicate which modules to import to access the variables
+;; used in this configuration.
 (use-modules (gnu))
-(use-modules (gnu system nss))
-(use-modules (gnu services networking))
-(use-modules (gnu services ssh))
-(use-modules (gnu services virtualizations))
-(use-modules (gnu packages))
+(use-modules (nongnu packages linux))
+(use-service-modules cups desktop networking ssh xorg)
 
 (operating-system
-  (host-name "moondust")
+  (kernel linux)
+  (firmware (list linux-firmware))
+  (locale "en_GB.utf8")
   (timezone "Europe/London")
-  (locale "en_UK.utf8")
+  (keyboard-layout (keyboard-layout "gb" "altgr-intl" #:model "thinkpad"))
+  (host-name "moondust")
 
-  ;; Choose UK English keyboard layout.  The "altgr-intl"
-  ;; variant provides dead keys for accented characters.
-  (keyboard-layout (keyboard-layout "uk" "altgr-intl" #:model "thinkpad"))
+  ;; The list of user accounts ('root' is implicit).
+  (users (cons* (user-account
+                  (name "pleb")
+                  (comment "Pleb")
+                  (group "users")
+                  (home-directory "/home/pleb")
+                  (supplementary-groups '("wheel" "netdev" "audio" "video")))
+                %base-user-accounts))
 
-  ;; Use the UEFI variant of GRUB with the EFI System
-  ;; Partition mounted on /boot/efi.
+  ;; Packages installed system-wide.  Users can also install packages
+  ;; under their own account: use 'guix search KEYWORD' to search
+  ;; for packages and 'guix install PACKAGE' to install a package.
+  (packages (append (list (specification->package "nss-certs"))
+                    %base-packages))
+
+  ;; Below is the list of system services.  To search for available
+  ;; services, run 'guix system search KEYWORD' in a terminal.
+  (services
+   (append (list (service network-manager-service-type)
+                 (service wpa-supplicant-service-type)
+                 (service ntp-service-type)
+                 (service gpm-service-type))
+
+           ;; This is the default list of services we
+           ;; are appending to.
+           %base-services))
   (bootloader (bootloader-configuration
-               (bootloader grub-efi-bootloader)
-               (targets '("/boot/efi"))
-               (keyboard-layout keyboard-layout)))
+                (bootloader grub-efi-bootloader)
+                (targets (list "/boot/efi"))
+                (keyboard-layout keyboard-layout)))
+  (mapped-devices (list (mapped-device
+                          (source (uuid
+                                   "cd108cc8-6779-45d1-89b8-ebeed6c8b79d"))
+                          (target "system-home")
+                          (type luks-device-mapping))
+                        (mapped-device
+                          (source (uuid
+                                   "fb4dfa22-42a5-4279-9417-45c3d5dc9859"))
+                          (target "system-root")
+                          (type luks-device-mapping))))
 
-  (file-systems (append
-                 (list (file-system
-                         (device (file-system-label "system-root"))
-                         (mount-point "/")
-                         (type "ext4"))
+  ;; The list of file systems that get "mounted".  The unique
+  ;; file system identifiers there ("UUIDs") can be obtained
+  ;; by running 'blkid' in a terminal.
+  (file-systems (cons* (file-system
+                         (mount-point "/home")
+                         (device "/dev/mapper/system-home")
+                         (type "btrfs")
+                         (dependencies mapped-devices))
                        (file-system
-                         (device "/dev/sda1")
+                         (mount-point "/")
+                         (device "/dev/mapper/system-root")
+                         (type "btrfs")
+                         (dependencies mapped-devices))
+                       (file-system
                          (mount-point "/boot/efi")
-                         (type "vfat")))
-                 %base-file-systems))
-
-  ;; This is where user accounts are specified.  The "root"
-  ;; account is implicit, and is initially created with the
-  ;; empty password.
-  (users (cons (user-account
-                (name "pleb")
-                (group "users")
-
-                ;; Adding the account to the "wheel" group
-                ;; makes it a sudoer.  Adding it to "audio"
-                ;; and "video" allows the user to play sound
-                ;; and access the webcam.
-                (supplementary-groups '("wheel" ;; sudo
-										"netdev" ;; Network devices
-										"realtime" ;; Enable realtime scheduling
-										"lp" ;; Control bluetooth devices
-                                        "audio" ;; Control audio devices
-										"video"))) ;; Control video devices
-               %base-user-accounts))
-
-  ;; Add the 'realtime' group
-  (groups (cons (user-group (system? #t) (name "realtime"))
-                 %base-groups))
-
-  ;; Install bare-minimum system packages
-  (packages (append (map specification->package
-						 '("git"
-						   "ntfs-3g" ;; NTFS file systems
-						   "exfat-utils" ;; exFAT file systems
-						   "fuse-exfat" ;; Mount exFAT file systems
-						   "bluez" ;; Bluetooth protocol stack
-						   "bluez-alsa" ;; Bluetooth ALSA backend
-						   "tlp" ;; Power management tool
-						   "stow"
-						   "alacritty"
-						   "neovim"
-						   "emacs"
-						   "nss-certs")) ;; SSL root certificates
-					%base-packages))
-
-  ;; Add services to the baseline: a DHCP client and
-  ;; an SSH server.
-  (services (append (list (service dhcp-client-service-type)
-                          (service openssh-service-type
-                                   (openssh-configuration
-                                    (openssh openssh-sans-x)
-                                    (port-number 2222))))
-                    %base-services)))
+                         (device (uuid "5FDA-C9EA"
+                                       'fat32))
+                         (type "vfat")) %base-file-systems)))
