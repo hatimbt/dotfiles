3ce36c3fc074115041636c0c72edc0e1c5223254
Hatim Thayyil <hatim@thayyil.net>
2024-03-11 23:04:03 +0000

    add neovim diagnostic config
    - Remove virtual text

--- guix/files/nvim/init.lua
+++ guix/files/nvim/init.lua
@@ -1,4 +1,5 @@
 require('options')
 require('keymaps')
+require('diagnostic')
 require('plugins')
 require('plugins.config')
---
+++ guix/files/nvim/lua/diagnostic.lua
@@ -0,0 +1,7 @@
+-- Configuration of the Neovim Diagnsotic API
+-- 
+-- https://neovim.io/doc/user/diagnostic.html#vim.diagnostic.config()
+
+vim.diagnostic.config({
+	virtual_text = false,
+})
