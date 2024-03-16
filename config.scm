20daf1488ebce04179ca80afd4d6f45395eb385f
Hatim Thayyil <hatim@thayyil.net>
2024-03-16 09:51:10 +0000

    chore formatting

--- config.scm
+++ config.scm
@@ -1,11 +1,9 @@
 (define-module (config)
   #:use-module (rde features)
-
-  #:use-module (thayyil systems tycho)
-
   #:use-module (srfi srfi-1)
-  #:use-module (ice-9 match))
+  #:use-module (ice-9 match)
 
+  #:use-module (thayyil systems tycho))
 
 (define tycho-he
   (rde-config-home-environment tycho-config))
